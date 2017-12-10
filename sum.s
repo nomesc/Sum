e3546615d584f210804227ae9585857cebe7c525
nomesc <33726880+nomesc@users.noreply.github.com>
2017-12-10 13:32:05 +0200

    Sum of the elements of an array written in MIPS assembly
    The sum and the array are stored in the memory.

--- sum.s
+++ sum.s
@@ -1,29 +1,27 @@
 .data
+sum: .space 4
  v:.word 2, 1, 3, 2, 3  # the array
- n:.word 5              # how many elements it has
+ n:.word 5              # number of elements
  i:.space 4             
-sum:.space 4
+
 .text
 main:
- sw $zero,i             # i:=0
- sw $zero,sum           # sum=0
- lw $t0,n
- beq $t0,$zero,exit     #if there are no elemnts exit
+ sw $zero,i    # i:=0
+ move $t4,$zero
 #loop
 entry:
  lw $t0,i
  lw $t1,n
- bge $t0,$t1,exit       # if i>=n exit
+ bge $t0,$t1,exit  # if (i>=n) exit
  add $t0,$t0,$t0
- add $t0,$t0,$t0     
- lw $t0,v($t0)          # $t0:=v[i]
- lw $t1,sum
- add $t1,$t1,$t0
- sw $t1,sum
+ add $t0,$t0,$t0     # $t0=4*i
+ lw $t0,v($t0)       # $t0=v[i]
+ add $t4,$t4,$t0
  lw $t0,i
  addi $t0,$t0,1
- sw $t0,i               # i++
+ sw $t0,i            # i++
  j entry
 exit:
+sw $t4,sum
 li $v0,10
 syscall
